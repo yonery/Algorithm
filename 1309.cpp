3087080e46b9a0d04fc6ca925550d32e7d66943b
yonery <yonery9@gmail.com>
2017-09-04 22:56:23 +0900

    2017-09-04

---
+++ 1309.cpp
@@ -0,0 +1,34 @@
+// 1309번 동물원
+#include <iostream>
+#define mod 9901
+int dp[100001][4] = { 0 };
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	// X X -> 이 다음에 4가지 가능(1, 2, 3, 4)
+	dp[1][1] = 1;
+	// O X -> 이 다음에 2가지 가능(1, 3)
+	dp[1][2] = 1;
+	// X O -> 이 다음에 2가지 가능 (1, 2)
+	dp[1][3] = 1;
+	// O O  -> 이 다음에 1가지 가능 (1)
+	dp[1][4] = 1;
+	
+	
+	for (int i = 2; i <= N; i++) {
+		dp[i][1] = (dp[i-1][1] + dp[i - 1][2] + dp[i - 1][3]) % mod;
+		dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % mod;
+		dp[i][3] = (dp[i - 1][1] + dp[i - 1][2]) % mod;
+
+	}
+
+	int total = 0;
+	for (int i = 1; i <= 3; i++)
+		total = (dp[N][i] + total)%mod;
+
+	printf("%d", total);
+
+	return 0;
+}
---
+++ 9461.cpp
@@ -0,0 +1,24 @@
+// 9461번 파도반 수열
+
+#include <iostream>
+
+long long dp[101] = { 1, 1, 1, 1, 2, 2, };
+
+int main() {
+
+	int T;
+	scanf("%d", &T);
+
+	//f(N) = f(N-1) + f(N-5)
+	for (int i = 6; i <= 100; i++) {
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
+
+	for (int t = 0; t < T; t++) {
+		int N;
+		scanf("%d", &N);
+		printf("%lld\n", dp[N]);
+	}
+
+	return 0;
+}
