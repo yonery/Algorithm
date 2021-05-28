e0cb26ed028e41a0ad2b399236655bdd351cfc9f
yonery <yonery9@gmail.com>
2021-05-28 12:26:43 +0900

    Create 2212.py

---
+++ BOJ/2212.py
@@ -0,0 +1,24 @@
+# 	2212 - 센서  https://www.acmicpc.net/problem/2212
+N = int(input())
+K = int(input())
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+if K == 1:
+    print(arr[len(arr)-1] - arr[0])
+elif K >= N:
+    print(0)
+else:
+    dp = []
+    for i in range(1, N):
+        dp.append(arr[i]-arr[i-1])
+    dp.sort()
+    s = 0
+    for i in range(len(dp)-K+1):
+        s += dp[i]
+    print(s)
+
+
+# 탐욕알고리즘
+# 전체 구간에서 K-1개 만큼 구간 삭제 
