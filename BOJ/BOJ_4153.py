d4bc1bc1d8fc0a6578f5a8c70db749c1342b78e9
yonery <yonery9@gmail.com>
2023-12-21 14:49:23 +0900

    Create BOJ_4153.py

---
+++ BOJ/BOJ_4153.py
@@ -0,0 +1,12 @@
+# 4153 직각삼각형 (python)
+
+while True:
+        a = list(map(int, input().split()))
+        max_num = max(a)
+        if sum(a) == 0:
+                break
+        a.remove(max_num)
+        if a[0] ** 2 + a[1] ** 2 == max_num ** 2:
+                print('right')
+        else:
+                print('wrong')
